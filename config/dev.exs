d056a850097b939f514785c05c583066523c19f9
Luke Turner <github@luketurner.org>
2023-02-07 15:51:31 -0800

    add tailwindcss
    following instructions from https://tailwindcss.com/docs/guides/phoenix

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :habitsheet, HabitsheetWeb.Endpoint,
   secret_key_base: "S6tCHClnZ+9IzaCY8NPUiIcuBcm5lFdu8+IreGdXEjsAXb7ea3Lp0VpeRMou+wPB",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
