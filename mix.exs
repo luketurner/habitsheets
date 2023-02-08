f27ec9c6bee4d05bc367d8ef2e496adea8518892
Luke Turner <github@luketurner.org>
2023-02-07 19:19:35 -0800

    add os_mon app for os stats in live dash

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Habitsheet.MixProject do
   def application do
     [
       mod: {Habitsheet.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
