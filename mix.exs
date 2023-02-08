7c26d453e03365e20fd4cd41baba90b8e5f2b98b
Luke Turner <github@luketurner.org>
2023-02-07 19:26:54 -0800

    don't show os stats for :dev environment

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Habitsheet.MixProject do
   def application do
     [
       mod: {Habitsheet.Application, []},
-      extra_applications: [:logger, :runtime_tools, :os_mon]
+      extra_applications: [:logger, :runtime_tools] ++ if(Mix.env() == :dev, do: [], else: [:os_mon])
     ]
   end
 
