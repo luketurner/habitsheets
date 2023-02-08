ff71169f436656c4852903d7d41e4c5bfc0a84b5
Luke Turner <github@luketurner.org>
2023-02-07 19:19:15 -0800

    add ecto_psql_extras to see pg stats in live dash

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule Habitsheet.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
+      {:ecto_psql_extras, "~> 0.6"},
     ]
   end
 
