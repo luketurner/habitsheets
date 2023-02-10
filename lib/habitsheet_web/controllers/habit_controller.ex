1e6d779f54d935d4c81831eb590778c1ba992446
Luke Turner <github@luketurner.org>
2023-02-09 22:30:09 -0800

    after habit crud ops, redirect back to sheet

--- lib/habitsheet_web/controllers/habit_controller.ex
+++ lib/habitsheet_web/controllers/habit_controller.ex
@@ -11,10 +11,10 @@ defmodule HabitsheetWeb.HabitController do
 
   def create(conn, %{"habit" => habit_params, "sheet_id" => sheet_id}) do
     case Sheets.create_habit(Map.put(habit_params, "sheet_id", sheet_id)) do
-      {:ok, habit} ->
+      {:ok, _habit} ->
         conn
         |> put_flash(:info, "Habit created successfully.")
-        |> redirect(to: Routes.sheet_habit_path(conn, :show, habit.sheet_id, habit))
+        |> redirect(to: Routes.sheet_path(conn, :show, sheet_id))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset, sheet_id: sheet_id)
@@ -36,10 +36,10 @@ defmodule HabitsheetWeb.HabitController do
     habit = Sheets.get_habit!(id)
 
     case Sheets.update_habit(habit, habit_params) do
-      {:ok, habit} ->
+      {:ok, _habit} ->
         conn
         |> put_flash(:info, "Habit updated successfully.")
-        |> redirect(to: Routes.sheet_habit_path(conn, :show, habit.sheet_id, habit))
+        |> redirect(to: Routes.sheet_path(conn, :show, sheet_id))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", habit: habit, changeset: changeset, sheet_id: sheet_id)
@@ -52,6 +52,6 @@ defmodule HabitsheetWeb.HabitController do
 
     conn
     |> put_flash(:info, "Habit deleted successfully.")
-    |> redirect(to: Routes.sheet_habit_path(conn, :index, sheet_id))
+    |> redirect(to: Routes.sheet_path(conn, :show, sheet_id))
   end
 end
