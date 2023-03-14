1835a1a18f204b7fa2e2371362b2c62850a4304a
Luke Turner <github@luketurner.org>
2023-03-13 21:42:43 -0700

    add basic additional_data_spec editor for habits

--- lib/habitsheet_web/live/habit_editor.ex
+++ lib/habitsheet_web/live/habit_editor.ex
@@ -1,16 +1,20 @@
 defmodule HabitsheetWeb.Live.HabitEditor do
   use HabitsheetWeb, :live_view
 
+  alias Ecto.Changeset
+
   alias Habitsheet.Habits
+  alias Habitsheet.Habits.AdditionalDataSpec
   alias Habitsheet.Habits.Habit
 
   @impl true
   def mount(_params, _session, %{assigns: %{live_action: :new}} = socket) do
     new_habit = %Habit{}
+    changeset = Habits.habit_create_changeset(new_habit)
 
     {:ok,
      socket
-     |> assign(:changeset, Habits.habit_create_changeset(new_habit))
+     |> assign(:changeset, changeset)
      |> assign(:habit, new_habit)}
   end
 
@@ -43,11 +47,52 @@ defmodule HabitsheetWeb.Live.HabitEditor do
     save_habit(socket, socket.assigns.live_action, habit_params)
   end
 
+  @impl true
+  def handle_event("add_spec", _, socket) do
+    changeset = socket.assigns.changeset
+
+    # TODO -- autogenerate IDs better?
+    new_spec =
+      AdditionalDataSpec.changeset(%AdditionalDataSpec{}, %{
+        label: "Label me",
+        data_type: :numeric,
+        id: Ecto.UUID.generate()
+      })
+
+    existing_specs = Changeset.get_field(changeset, :additional_data_spec, [])
+
+    changeset =
+      Changeset.put_embed(changeset, :additional_data_spec, existing_specs ++ [new_spec])
+
+    {:noreply, socket |> assign(:changeset, changeset)}
+  end
+
+  @impl true
+  def handle_event("delete_spec", %{"id" => id}, socket) do
+    changeset = socket.assigns.changeset
+
+    existing_specs = Changeset.get_field(changeset, :additional_data_spec, [])
+    new_specs = Enum.reject(existing_specs, &(&1.id == id))
+
+    changeset = Changeset.put_embed(changeset, :additional_data_spec, new_specs)
+
+    {:noreply, socket |> assign(:changeset, changeset)}
+  end
+
   defp save_habit(socket, :edit, habit_params) do
-    case Habits.update_habit_as(
-           socket.assigns.current_user,
-           Habits.habit_update_changeset(socket.assigns.habit, habit_params)
-         ) do
+    changeset = Habit.update_changeset(socket.assigns.habit, habit_params)
+
+    # TODO -- this is necessary because if no additional_data_specs are specified,
+    # the habit_params, and therefore the changeset, won't have any changes. When
+    # in fact, we do want there to be a change, which is to empty out the list.
+    changeset =
+      if Changeset.get_change(changeset, :additional_data_spec) do
+        changeset
+      else
+        Changeset.put_embed(changeset, :additional_data_spec, [])
+      end
+
+    case Habits.update_habit_as(socket.assigns.current_user, changeset) do
       {:ok, _habit} ->
         {:noreply,
          socket
