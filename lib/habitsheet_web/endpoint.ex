9522174593809f580edd098fd44ddd25d45d7820
Luke Turner <github@luketurner.org>
2023-04-09 13:57:13 -0700

    add manifest.json to static assets allow-list

--- lib/habitsheet_web/endpoint.ex
+++ lib/habitsheet_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule HabitsheetWeb.Endpoint do
     at: "/",
     from: :habitsheet,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images favicon.ico robots.txt manifest.json)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
