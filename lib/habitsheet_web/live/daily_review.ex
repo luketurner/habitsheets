11082896dc89faf14773a003e590325ff20ce1d5
Luke Turner <github@luketurner.org>
2023-04-10 10:29:10 -0700

    reintroduce review timer

--- lib/habitsheet_web/live/daily_review.ex
+++ lib/habitsheet_web/live/daily_review.ex
@@ -5,6 +5,8 @@ defmodule HabitsheetWeb.Live.DailyReview do
   alias Habitsheet.Reviews
   alias Habitsheet.Reviews.DailyReview
 
+  @default_time_remaining Time.from_seconds_after_midnight(300)
+
   @impl true
   def mount(%{"date" => date_param} = _params, _session, socket) do
     socket = socket |> assign_review()
@@ -24,7 +26,10 @@ defmodule HabitsheetWeb.Live.DailyReview do
      |> assign(:date_param, date_param)
      |> assign_habits()
      |> assign_entries()
-     |> assign_review()}
+     |> assign_review()
+     |> assign(:time_remaining, @default_time_remaining)
+     |> assign(:default_time_remaining, @default_time_remaining)
+     |> assign(:timer, nil)}
   end
 
   @impl true
@@ -40,6 +45,36 @@ defmodule HabitsheetWeb.Live.DailyReview do
     else
       _ -> {:noreply, socket}
     end
+  end
+
+  @impl true
+  def handle_event("toggle_timer", _params, %{assigns: %{timer: timer}} = socket)
+      when not is_nil(timer) do
+    {:noreply, socket |> clear_timer()}
+  end
+
+  @impl true
+  def handle_event("toggle_timer", _params, socket) do
+    {:noreply, socket |> assign_timer()}
+  end
+
+  @impl true
+  def handle_event("restart_timer", _params, socket) do
+    {:noreply, socket |> assign(:time_remaining, @default_time_remaining) |> clear_timer()}
+  end
+
+  @impl true
+  def handle_info(:tick_timer, socket) do
+    # Don't decrement if the timer has already been removed.
+    # This prevents an extra second from ticking off after the user clicks pause.
+    new_time =
+      if socket.assigns.timer,
+        do: Time.add(socket.assigns.time_remaining, -1),
+        else: socket.assigns.time_remaining
+
+    # prevent time from rolling over when it goes below midnight
+    new_time = if new_time > socket.assigns.time_remaining, do: ~T[00:00:00], else: new_time
+    {:noreply, socket |> assign(:time_remaining, new_time)}
   end
 
   def assign_habits(socket) do
@@ -78,5 +113,28 @@ defmodule HabitsheetWeb.Live.DailyReview do
     else
       _ -> socket
     end
+  end
+
+  defp assign_timer(socket) do
+    socket = socket |> clear_timer()
+
+    with {:ok, tref} <- :timer.send_interval(1000, :tick_timer) do
+      socket |> assign(:timer, tref)
+    else
+      # TODO
+      _ -> socket
+    end
+  end
+
+  defp clear_timer(socket) do
+    tref = socket.assigns[:timer]
+    if tref, do: :timer.cancel(tref)
+    socket |> assign(:timer, nil)
+  end
+
+  defp elapsed_time_percent(total, remaining) do
+    {total_secs, _} = Time.to_seconds_after_midnight(total)
+    {remaining_secs, _} = Time.to_seconds_after_midnight(remaining)
+    (total_secs - remaining_secs) * (100 / total_secs)
   end
 end
