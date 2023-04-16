54b3a8e2fd6833dc1d01ff440d9d222dd000a297
Luke Turner <github@luketurner.org>
2023-04-15 19:13:30 -0700

    fix manpage loading for deployed app

--- lib/habitsheet_web/live/live_helpers.ex
+++ lib/habitsheet_web/live/live_helpers.ex
@@ -172,12 +172,14 @@ defmodule HabitsheetWeb.LiveHelpers do
   end
 
   def load_manpage(manpage) do
+    manpage_dir = Application.app_dir(:habitsheet, "priv/manpage")
+    manpage_file = "#{manpage}.md"
     # TODO -- avoid running ls on every request
-    valid_files = File.ls!("priv/manpage")
+    valid_files = File.ls!(manpage_dir)
 
     # An allow-list of valid files is used to prevent attacks with ..
-    if Enum.member?(valid_files, "#{manpage}.md") do
-      Earmark.from_file!("priv/manpage/#{manpage}.md")
+    if Enum.member?(valid_files, manpage_file) do
+      Earmark.from_file!(Path.join(manpage_dir, manpage_file))
     else
       "Unknown manpage"
     end
