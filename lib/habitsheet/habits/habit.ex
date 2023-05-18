752908da68d3f7ce1e5f380f7d0076cb7a8eecec
Luke Turner <github@luketurner.org>
2023-05-17 18:04:13 -0700

    add some helper methods to habit schema

--- lib/habitsheet/habits/habit.ex
+++ lib/habitsheet/habits/habit.ex
@@ -78,4 +78,60 @@ defmodule Habitsheet.Habits.Habit do
 
   def habit_types(), do: @habit_types
   def habit_senses(), do: @habit_senses
+
+  @doc """
+  Returns true if the habit recurs on a given day. Should always return true if no recurrences are defined.
+
+  # Examples
+
+      iex> Habit.recurs_on(%Habit{recurrence: []}, ~D[2023-01-01])
+      true
+
+      iex> Habit.recurs_on(%Habit{recurrence: nil}, ~D[2023-01-01])
+      true
+
+      iex> Habit.recurs_on(%Habit{recurrence: [%RecurringInterval{type: :weekly, every: 1, start: ~D[2023-01-01]}]}, ~D[2023-01-01])
+      true
+
+      iex> Habit.recurs_on(%Habit{recurrence: [%RecurringInterval{type: :weekly, every: 1, start: ~D[2023-01-01]}]}, ~D[2023-01-02])
+      false
+
+      iex> Habit.recurs_on(%Habit{recurrence: [%RecurringInterval{type: :weekly, every: 1, start: ~D[2023-01-01]}]}, ~D[2023-01-08])
+      true
+  """
+  def recurs_on(%__MODULE__{} = habit, %Date{} = date) do
+    recurrence = Map.get(habit, :recurrence, [])
+
+    is_nil(recurrence) or Enum.empty?(recurrence) or
+      Enum.any?(recurrence, &RecurringInterval.recurs_on(&1, date))
+  end
+
+  @doc """
+  Returns true if the habit would be expired, given the previous date the habit was performed
+  and the current date.
+
+  # Examples
+
+      iex> Habit.expired(%Habit{expiration: 2}, ~D[2022-01-01], ~D[2022-01-04])
+      true
+
+      iex> Habit.expired(%Habit{expiration: 2}, ~D[2022-01-01], ~D[2022-01-03])
+      true
+
+      iex> Habit.expired(%Habit{expiration: 2}, ~D[2022-01-01], ~D[2022-01-02])
+      false
+
+      iex> Habit.expired(%Habit{expiration: 1}, ~D[2022-01-01], ~D[2022-01-02])
+      true
+
+      iex> Habit.expired(%Habit{}, ~D[2022-01-01], ~D[2022-01-02])
+      false
+  """
+  def expired(
+        %__MODULE__{} = habit,
+        %Date{} = last_habit_date,
+        %Date{} = check_date
+      ) do
+    Date.diff(check_date, last_habit_date) >= Map.get(habit, :expiration, 1)
+  end
 end
---
+++ test/habitsheet/habits/habit_test.exs
@@ -0,0 +1,6 @@
+defmodule Habitsheet.Habits.HabitTest do
+  use ExUnit.Case, async: true
+  alias Habitsheet.Habits.Habit
+  alias Habitsheet.Habits.RecurringInterval
+  doctest Habit
+end
